1a43eeb3b38a0db85e5c199d84b4db1e552fa7c8
Maggie-Bowen <margaretbowen35@gmail.com>
2023-09-25 15:16:34 +0200

    added filler code to about page to test deploy

---
+++ pages/About.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css';
+
+function About() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>About Maggie Bowen</h1>
+      <p>haven&#39;t made about page yet</p>
+    </div>
+  );
+}
+
+export default About;
