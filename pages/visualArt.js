d435f0cd66b1f55836abde3d12973db095714cf2
Maggie-Bowen <margaretbowen35@gmail.com>
2023-10-27 18:48:33 +0200

    deleted mock json

--- pages/visualArt.js
+++ pages/visualArt.js
@@ -30,11 +30,11 @@ function VisualArt() {
       <h1 className={styles.pageTitle}>Visual Art</h1>
       <Navigation />
       <div className={styles.imageGrid}>
-        <div className={styles.apiImages}>
-          {imageList.map((image) => (
-            <Image fill src={image.url} alt={image.public_id} />
-          ))}
-        </div>
+        {/* <div className={styles.apiImages}> */}
+        {imageList.map((image) => (
+          <Image src={image.url} alt={image.public_id} width={image.width} height={image.height} />
+        ))}
+        {/* </div> */}
       </div>
       <div>
         {nextCursor && (
