9beb3ef010a78412a096557fbf6c08567ea3b557
Maggie-Bowen <margaretbowen35@gmail.com>
2024-01-08 22:02:48 +0100

    try/catch to hopefully find API endpoint error

--- pages/api/cloudinary.js
+++ pages/api/cloudinary.js
@@ -1,30 +1,66 @@
 /* eslint-disable import/prefer-default-export */
+
 export async function search(options = {}) {
-  const params = {
-    ...options,
-  };
+  try {
+    const params = {
+      ...options,
+    };
 
-  if (options.nextCursor) {
-    params.next_cursor = options.nextCursor;
-    delete params.nextCursor;
-  }
+    if (options.nextCursor) {
+      params.next_cursor = options.nextCursor;
+      delete params.nextCursor;
+    }
 
-  const paramString = Object.keys(params)
-    .map((key) => `${key}=${encodeURIComponent(params[key])}`)
-    .join('&');
-  const results = await fetch(
-    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/search?${paramString}`,
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          `${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}`,
-        ).toString('base64')}`,
+    const paramString = Object.keys(params)
+      .map((key) => `${key}=${encodeURIComponent(params[key])}`)
+      .join('&');
+
+    const results = await fetch(
+      `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/search?${paramString}`,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}`,
+          ).toString('base64')}`,
+        },
       },
-    },
-  ).then((r) => r.json());
+    ).then((r) => r.json());
 
-  return results;
+    return results;
+  } catch (error) {
+    // Handle the error here, you can log it or perform any other necessary actions
+    console.error('An error occurred in the search function:', error);
+    // You may choose to throw the error again or return a default value
+    throw error;
+  }
 }
+
+// export async function search(options = {}) {
+//   const params = {
+//     ...options,
+//   };
+
+//   if (options.nextCursor) {
+//     params.next_cursor = options.nextCursor;
+//     delete params.nextCursor;
+//   }
+
+//   const paramString = Object.keys(params)
+//     .map((key) => `${key}=${encodeURIComponent(params[key])}`)
+//     .join('&');
+//   const results = await fetch(
+//     `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/search?${paramString}`,
+//     {
+//       headers: {
+//         Authorization: `Basic ${Buffer.from(
+//           `${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}`,
+//         ).toString('base64')}`,
+//       },
+//     },
+//   ).then((r) => r.json());
+
+//   return results;
+// }
 
 export function mapImageResources(resources) {
   return resources.map((resource) => {
